3edc2bb0740e9c751583c3a9235a9187f0a2bc2a
mehranjafariii <97957377+mehranjafariii@users.noreply.github.com>
2025-03-23 18:58:34 +0330

    Add files via upload

---
+++ EPSR 2024/Integral controller/PSCCPIControllerATM.m
@@ -0,0 +1,448 @@
+%% %% This file solves the RTE PI controller using ATM (Analog)
+clear all
+clc
+tic
+% This parameter chooses the predictor method. It can take values 4 and 5. 4 is for the first-order Adams-Bashford, and 5 is for the second-order Adams-Bashford. 
+cp=5; %Predictor Choice
+% This parameter chooses the corrector method. It can take values 2 and 3. 2 is for the second-order BDF, and 3 is for the second-order Adams-Moulton. 
+cc=3; %Corrector Choice
+%This parameters chooses the quantization method
+%1 uniform quantization, 2 Nonuniform quantization 
+cq=1; %Quantization method
+
+t=0; %Starting point of the integration
+h=0.001; %The first time-step size
+holdd=h;
+hmin=0.001; %The minimum acceptable time-step size
+hmax=1; %The maximum acceptable time-step size
+
+tsim=75; %Time of the simulation
+%Initial state variables values
+
+dae=[3 0];
+x=0;
+xxx=[0;0];
+xx=[x;xxx];
+s=size(xx);
+s=s(1);
+xpre1=xx;
+xpre2=xx;
+times(1)=t;
+xhist(1:s,1)=xx; %Allocating a space to save the results
+hhist(1,1)=h;
+
+iterationsc=0; %Allocating a space to save the number of Newoton iterations
+tsc=0; %Number of time steps
+mtsc=0; %Number of accepted time steps with the manimim value
+
+k=1;
+ETOL=3e-4; %The tolerance to compare with the error estimate
+
+save=0;
+while t<tsim
+        %Setting old times
+    if k>2
+        tpre3=tpre2;
+    else
+        tpre3=0;
+    end
+    if k>1
+        tpre2=t-holdd;
+    else
+        tpre2=0;    
+    end
+    flagg=0;
+    tpre1=t;
+    t=t+h;
+    
+    %Setting old solutions
+    if k>2
+        xpre3=xhist(1:s,k-2);
+    else
+        xpre3=0;
+    end
+    if k>1
+    xpre2=xhist(1:s,k-1);
+    else
+        xpre2=xpre1;
+    end
+    xpre1=xhist(1:s,k);
+    k=k+1;
+    %Summation of the controllers' signal
+    times(k)=t;
+
+    [alpha0,alpha1,alpha2,betha1,betha2]=coefs(cp,h,holdd); %calculating the coefficients of the predictor
+    xx0=predictor(xx,xpre1,t,tpre1,h,betha1,betha2,dae); %predictor
+    [alpha0,alpha1,alpha2,betha1,betha2]=coefs(cc,h,holdd); %calculating the coefficients of the corrector
+    [iterations,xx]=NewtonNL(h,xx0,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,t,dae); %The main function (Newton solver)
+    [dn,r]=lote(cp,cc,xx0,xx,h,holdd); %Error estimate
+    iterationsc=iterations+iterationsc; %Counting the number of Newton iterations
+
+   %Calculating the new time step and saving the results for the current time step         
+   hpre=h;
+    if dn>ETOL
+        hc=[hmin,0.5*h;];
+        h=max(hc);
+        if h==hmin
+            mtsc=mtsc+1;
+            tsc=tsc+1;
+            disp('The minimum time step is accepted')
+            xhist(1:s,k)=xx;
+            x2=xx(2,1);
+            if k==2
+                hhist(1,1)=h;
+                hhist(2,1)=dn;
+                hhist(3,1)=r;
+            end
+            hhist(1,k)=h;
+            hhist(2,k)=dn;
+            hhist(3,k)=r;
+            holdd=hpre;
+        else
+                t=t-hpre;
+                tpre1=tpre2;
+                if k>2
+                    tpre2=tpre3;
+                else
+                    tpre2=0;
+                end
+                xpre1=xpre2;
+                if k>2
+                    xpre2=xpre3;
+                else
+                    xpre2=0;
+                end
+                if flagg==1
+                    tg=tg-T;
+                end
+                k=k-1;
+
+        end
+    else
+        hc=[1.25*h,hmax];
+        h=min(hc);
+        tsc=tsc+1;
+        xhist(1:s,k)=xx;
+        x2=xx(2,1);
+        if k==2
+            hhist(1,1)=h;
+            hhist(2,1)=dn;
+            hhist(3,1)=r;
+        end
+        hhist(1,k)=h;
+        hhist(2,k)=dn;
+        hhist(3,k)=r;
+        holdd=hpre;
+    end
+
+
+end
+toc
+
+
+%Plotting the results
+figure(1)
+plot(times,xhist(3,:),':+')
+hold on
+% plot(times,xhist(6,:),'--')
+% plot(times,xhist(2,:))
+xlabel('Time (s)','FontSize', 24)
+ylabel('Controller output','FontSize', 24)
+set(gca,'FontSize',18)
+legend('y','w','u','x','Location','northeast')
+set(gca, 'FontName', 'Times New Roman')
+xlim([0 tsim])
+figure_property.units = 'inches';
+figure_property.format = 'pdf';
+figure_property.Preview= 'none';
+figure_property.Width= '16'; % Figure width on canvas
+figure_property.Height= '9'; % Figure height on canvas
+figure_property.Units= 'inches';
+figure_property.Color= 'rgb';
+figure_property.Background= 'w';
+figure_property.FixedfontSize= '12';
+figure_property.ScaledfontSize= 'auto';
+figure_property.FontMode= 'scaled';
+figure_property.FontSizeMin= '12';
+figure_property.FixedLineWidth= '1';
+figure_property.ScaledLineWidth= 'auto';
+figure_property.LineMode= 'none';
+figure_property.LineWidthMin= '0.1';
+figure_property.FontName= 'Times New Roman';% Might want to change this to something that is available
+figure_property.FontWeight= 'auto';
+figure_property.FontAngle= 'auto';
+figure_property.FontEncoding= 'latin1';
+figure_property.PSLevel= '3';
+figure_property.Renderer= 'painters';
+figure_property.Resolution= '600';
+figure_property.LineStyleMap= 'none';
+figure_property.ApplyStyle= '0';
+figure_property.Bounds= 'tight';
+figure_property.LockAxes= 'off';
+figure_property.LockAxesTicks= 'off';
+figure_property.ShowUI= 'off';
+figure_property.SeparateText= 'off';
+chosen_figure=gcf;
+set(chosen_figure,'PaperUnits','inches');
+set(chosen_figure,'PaperPositionMode','auto');
+set(chosen_figure,'PaperSize',[str2num(figure_property.Width) str2num(figure_property.Height)]); % Canvas Size
+set(chosen_figure,'Units','inches');
+if save==1
+    hgexport(gcf,'C:\Users\mjafa\Desktop\Coding tutorial\MATLAB\PI controller paper\PIAnalogFeedback.pdf',figure_property);
+end
+
+figure(2)
+plot(times,xhist(3,:),':')
+xlim([56 66])
+ylim([0.77 1.06])
+hold on
+
+% figure(2)
+% plot(times,xhist(4,:))
+% hold on
+% xlabel('Time (s)','FontSize', 24)
+% ylabel('System output','FontSize', 24)
+% set(gca,'FontSize',18)
+% % legend('y','w','u','x','Location','northeast')
+% set(gca, 'FontName', 'Times New Roman')
+% xlim([0 tsim])
+% figure_property.units = 'inches';
+% figure_property.format = 'pdf';
+% figure_property.Preview= 'none';
+% figure_property.Width= '16'; % Figure width on canvas
+% figure_property.Height= '9'; % Figure height on canvas
+% figure_property.Units= 'inches';
+% figure_property.Color= 'rgb';
+% figure_property.Background= 'w';
+% figure_property.FixedfontSize= '12';
+% figure_property.ScaledfontSize= 'auto';
+% figure_property.FontMode= 'scaled';
+% figure_property.FontSizeMin= '12';
+% figure_property.FixedLineWidth= '1';
+% figure_property.ScaledLineWidth= 'auto';
+% figure_property.LineMode= 'none';
+% figure_property.LineWidthMin= '0.1';
+% figure_property.FontName= 'Times New Roman';% Might want to change this to something that is available
+% figure_property.FontWeight= 'auto';
+% figure_property.FontAngle= 'auto';
+% figure_property.FontEncoding= 'latin1';
+% figure_property.PSLevel= '3';
+% figure_property.Renderer= 'painters';
+% figure_property.Resolution= '600';
+% figure_property.LineStyleMap= 'none';
+% figure_property.ApplyStyle= '0';
+% figure_property.Bounds= 'tight';
+% figure_property.LockAxes= 'off';
+% figure_property.LockAxesTicks= 'off';
+% figure_property.ShowUI= 'off';
+% figure_property.SeparateText= 'off';
+% chosen_figure=gcf;
+% set(chosen_figure,'PaperUnits','inches');
+% set(chosen_figure,'PaperPositionMode','auto');
+% set(chosen_figure,'PaperSize',[str2num(figure_property.Width) str2num(figure_property.Height)]); % Canvas Size
+% set(chosen_figure,'Units','inches');
+% if save==1
+%     hgexport(gcf,'C:\Users\mjafa\Desktop\Coding tutorial\MATLAB\PI controller paper\PIAnalogFeedback2.pdf',figure_property);
+% end
+
+% figure(3)
+% plot(times,xhist(2,:)-xhist(4,:))
+% hold on
+% xlabel('Time','FontSize', 24)
+% ylabel('Output','FontSize', 24)
+% set(gca,'FontSize',18)
+
+% figure(5)
+% bar(times,hhist(5,:))
+% % xlim([4.8 5.6])
+% hold on
+% xlabel('Time','FontSize', 24)
+% ylabel('Number of iterations','FontSize', 24)
+% set(gca,'FontSize',18)
+
+figure(11)
+plot(times,hhist(1,:))
+hold on
+xlabel('Time','FontSize', 24)
+ylabel('Step Size','FontSize', 24)
+set(gca,'FontSize',18)
+
+figure(12)
+plot(times,hhist(2,:))
+xlabel('Time','FontSize', 24)
+ylabel('Error Estimate','FontSize', 24)
+set(gca,'FontSize',18)
+hold on
+
+function [iterations,xx]=NewtonNL(h,xx,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,t,dae)
+NTOL=1e-4; %Tolerance for the Newton solver
+iterations=0;
+a=1;
+    while (iterations<10) && (a>NTOL)
+    xxpre=xx;
+    iterations=iterations+1;
+    [J]=Jacob(h,xx,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,t,dae); %calculating the Jacobian
+    yy=xx-J\func(h,xx,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,t,dae); %Newton
+    xx=yy;
+    a=abs(xx-xxpre);
+    a=max(a);
+    if iterations==10
+        disp('Did not converged')
+        t=t
+    end
+    end
+%     if xx(6,1)>2.2
+%         xx(6,1)=2.2;
+%     elseif xx(6,1)<-2.2
+%         xx(6,1)=-2.2;
+%     end
+end
+
+% This function computes the Jacobian
+function [J]=Jacob(h,xx,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,t,dae)
+s=size(xx);
+s=s(1);
+delta=1e-6;
+i=0;
+J=zeros(s,s);
+while i<s
+    i=i+1;
+    f0=func(h,xx,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,t,dae);
+    yy=xx;
+    yy(i)=xx(i)+delta;
+    f1=func(h,yy,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,t,dae);
+    J(:,i)=(f1-f0)/delta;
+end
+end
+
+% This function determines the predictor formulation
+function [xxp]=predictor(xx,xpre1,t,tpre1,h,betha1,betha2,dae)
+    xxp1=xx+h*(((betha1)*evaleval(xx,t))+((betha2)*evaleval(xpre1,tpre1)));
+    xxp2=xx+evaleval(xx,t);
+
+if dae(1,1)==0
+    xxp=xxp2;
+elseif dae(1,2)==0
+    xxp=xxp1;
+else
+    xxp=[xxp1(1:dae(1,1),1);xxp2(dae(1,1)+1:end,1)];
+end
+end
+
+
+%This function determines the corrector formulation
+function [f]=func(h,xx,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,t,dae) 
+    f1=h*(betha1*evaleval(xx,t)+betha2*evaleval(xpre1,tpre1))+(alpha0*xx)+(alpha1*xpre1)+(alpha2*xpre2);
+    f2=evaleval(xx,t);
+
+if dae(1,1)==0
+    f=f2;
+elseif dae(1,2)==0
+    f=f1;
+else
+    f=[f1(1:dae(1,1),1);f2(dae(1,1)+1:end,1)];
+end
+end
+
+%This function computes the coefficients based on the step sizes
+function [alpha0,alpha1,alpha2,betha1,betha2]=coefs(c,h,holdd)
+if c==1
+    % corrector coefficients
+    % BDF-1
+    alpha1=1;
+    alpha2=0;
+    alpha0=-1;
+    betha1=1;
+    betha2=0;
+
+elseif c==2
+    %corrector coefficients
+    % BDF-2
+    alpha1=1+h/holdd;
+    alpha2=-(h^2)/((h+holdd)*holdd);
+    alpha0=-(2*h+holdd)/(h+holdd);
+    betha1=1;
+    betha2=0;
+
+elseif c==3
+    %corrector coefficients
+    % Adams-Moulton-2
+    alpha1=1;
+    alpha2=0;
+    alpha0=-1;
+    betha1=1/2;
+    betha2=1/2; 
+
+elseif c==4
+    %predictor coefficients
+    %AB-1
+    alpha1=0;
+    alpha2=0;
+    alpha0=0;
+    betha1=1;
+    betha2=0;
+    
+elseif c==5
+    %predictor coefficients
+    %AB-1
+    alpha1=0;
+    alpha2=0;
+    alpha0=0;
+    betha1=1+(h/(holdd*2));
+    betha2=-h/(holdd*2);
+end
+end
+
+%This function computes the error estimate for predictor corrector methods (Milne's estimate)
+function [dn,r]=lote(cp,cc,yp,yy,h,holdd)
+global ETOL
+ETOL=3e-6;
+if cc==1 && cp==4
+    dn=(yy-yp)*(-1/2)/((-1/2)-(1/2));
+    dn=max(abs(dn(:)));
+    r=0.9*ETOL/(dn*h);
+    r=nthroot(abs(r),2);
+% elseif cc==1 && cp==5
+%     dn=(yy-yp)/3*(1+holdd/h);
+%     r=0.9*ETOL/(dn*h);
+%     r=nthroot(r,3);  
+% elseif cc==2 && cp==4
+%     dn=(yy-yp)/3*(1+holdd/h);
+%     r=0.9*ETOL/(dn*h);
+%     r=nthroot(r,3);
+elseif cc==2 && cp==5
+    dn=(yy-yp)*(-2/9)/((-2/9)-(5/12));
+    dn=max(abs(dn(1:4,1)));
+    r=0.9*ETOL/(dn*h);
+    r=nthroot(r,3);    
+% elseif cc==3 && cp==4
+%     dn=(yy-yp)/3*(1+holdd/h);
+%     r=0.9*ETOL/(dn*h);
+%     r=nthroot(r,3);                            
+elseif cc==3 && cp==5
+    dn=(yy-yp)/(3*(1+holdd/h));
+    dn=max(abs(dn(:)));
+    r=0.9*ETOL/dn;
+    r=nthroot(r,3);
+end
+end
+
+%This function defines the system's formulation
+function [f]=evaleval(xx,t)
+u=1;
+ki=0.07;
+%Variable assignment
+xxx=[xx(2,1);xx(3,1)];
+f1=ki*(u-xx(3,1)); 
+[A,B]=basedmatrix;
+f2=A*xxx+B*xx(1,1);
+f=[f1;f2];
+end
+
+%System's eigenvalues
+function [A,B]=basedmatrix
+a=-0.2;
+b=0.9;
+A=[2*a sqrt(a^2+b^2);-sqrt(a^2+b^2) 0];
+B=[-sqrt(a^2+b^2) 0]';
+end
---
+++ EPSR 2024/Integral controller/PSCCPIControllerSRM.m
@@ -0,0 +1,386 @@
+%% %% This file solves the RTE PI controller using SRM
+%%%% Please set the parameters based on the guidance provided in the Readme.md file 
+
+clear all
+clc
+% This parameter chooses the predictor method. It can take values 4 and 5. 4 is for the first-order Adams-Bashford, and 5 is for the second-order Adams-Bashford. 
+cp=5; %Predictor Choice
+% This parameter chooses the corrector method. It can take values 2 and 3. 2 is for the second-order BDF, and 3 is for the second-order Adams-Moulton. 
+cc=3; %Corrector Choice
+%This parameters chooses the quantization method
+%1 uniform quantization, 2 Nonuniform quantization 
+cq=1; %Quantization method
+
+t=0; %Starting point of the integration
+h=0.001; %The first time-step size
+holdd=h;
+hmin=0.001; %The minimum acceptable time-step size
+hmax=1; %The maximum acceptable time-step size
+
+tsim=75; %Time of the simulation
+%Initial state variables values
+x=0; 
+y=0;
+xx=[x;y];
+T=0.1; %Time of sampling by the controller
+G=0.07; %Gain of the controller
+u=1; %Input 
+
+s=size(xx);
+s=s(1);
+xpre1=xx;
+xpre2=xx;
+times(1)=t;
+xhist(1:s,1)=xx; %Allocating a space to save the results
+
+iterationsc=0; %Allocating a space to save the number of Newoton iterations
+tsc=0; %Number of time steps
+mtsc=0; %Number of accepted time steps with the manimim value
+etpre=0;
+et=0;
+tg=T; %Time of the events
+k=1;
+ETOL=3e-4; %The tolerance to compare with the error estimate
+
+
+while t<tsim
+        %Setting old times
+    if k>2
+        tpre3=tpre2;
+    else
+        tpre3=0;
+    end
+    if k>1
+        tpre2=t-holdd;
+    else
+        tpre2=0;    
+    end
+    flagg=0;
+    tpre1=t;
+    t=t+h;
+    
+    %Setting old solutions
+    if k>2
+        xpre3=xhist(1:s,k-2);
+    else
+        xpre3=0;
+    end
+    if k>1
+    xpre2=xhist(1:s,k-1);
+    else
+        xpre2=xpre1;
+    end
+    xpre1=xhist(1:s,k);
+    
+    %Setting new and old controllers's signal
+    if t<T
+        et=0;
+        etpre=0;
+        etpre2=0;
+    elseif tg>tpre1 && tg<=t+0.00000001
+        etpre2=etpre;
+        etpre=et;
+        et=et+(G*T*(u-x2)); %Controller's equation
+%         et=min(et,0.9); %upper limnit anti wind up
+%         et=max(et,-0.9); %lower limnit anti wind up
+        et=quz(et,cq);
+        t=tg;
+        h=t-tpre1; %Zero-crossing
+        tg=tg+T;
+        flagg=1;
+    else
+        etpre2=etpre;
+        etpre=et;
+        et=et;
+        tg=tg;
+    end
+    
+    k=k+1;
+    times(k)=t;
+
+    [alpha0,alpha1,alpha2,betha1,betha2]=coefs(cp,h,holdd); %calculating the coefficients of the predictor
+    xx0=predictor(xx,xpre1,et,etpre,t,tpre1,h,betha1,betha2); %predictor
+    [alpha0,alpha1,alpha2,betha1,betha2]=coefs(cc,h,holdd); %calculating the coefficients of the corrector
+    [iterations,xx]=NewtonNL(h,xx0,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,et,etpre,t); %The main function (Newton solver)
+    [dn,r]=lote(cp,cc,xx0,xx,h,holdd); %Error estimate
+    iterationsc=iterations+iterationsc; %Counting the number of Newton iterations
+
+   %Calculating the new time step and saving the results for the current time step         
+   hpre=h;
+    if dn>ETOL
+        hc=[hmin,0.5*h;];
+        h=max(hc);
+        if h==hmin
+            mtsc=mtsc+1;
+            tsc=tsc+1;
+            disp('The minimum time step is accepted')
+            xhist(1:s,k)=xx;
+            x2=xx(2,1);
+            if k==2
+                hhist(1,1)=h;
+                hhist(2,1)=dn;
+                hhist(3,1)=r;
+                hhist(4,1)=et;
+            end
+            hhist(1,k)=h;
+            hhist(2,k)=dn;
+            hhist(3,k)=r;
+            hhist(4,k)=et;
+            holdd=hpre;
+        else
+                t=t-hpre;
+                tpre1=tpre2;
+                if k>2
+                    tpre2=tpre3;
+                else
+                    tpre2=0;
+                end
+                xpre1=xpre2;
+                if k>2
+                    xpre2=xpre3;
+                else
+                    xpre2=0;
+                end
+                if flagg==1
+                    tg=tg-T;
+                    et=etpre;
+                    etpre=etpre2;
+                end
+                k=k-1;
+
+        end
+    else
+        hc=[1.25*h,hmax];
+        h=min(hc);
+        tsc=tsc+1;
+        xhist(1:s,k)=xx;
+        x2=xx(2,1);
+        if k==2
+            hhist(1,1)=h;
+            hhist(2,1)=dn;
+            hhist(3,1)=r;
+            hhist(4,1)=et;
+        end
+        hhist(1,k)=h;
+        hhist(2,k)=dn;
+        hhist(3,k)=r;
+        hhist(4,k)=et;
+        holdd=hpre;
+    end
+
+
+end
+
+%Plotting the results
+figure(1)
+plot(times,xhist(2,:),'k')
+hold on
+
+figure(2)
+plot(times,xhist(2,:))
+xlim([56 66])
+ylim([0.77 1.06])
+hold on
+
+% figure(2)
+% plot(xhist(1,:),xhist(3,:))
+
+figure(11)
+plot(times,hhist(1,:),'k')
+hold on
+
+
+figure(12)
+plot(times,hhist(2,:))
+hold on
+
+
+
+function [iterations,xx]=NewtonNL(h,xx,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,et,etpre,t)
+NTOL=1e-4; %Tolerance for the Newton solver
+iterations=0;
+a=1;
+    while (iterations<10) && (a>NTOL)
+    xxpre=xx;
+    iterations=iterations+1;
+    [J]=Jacob(h,xx,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,et,etpre,t); %calculating the Jacobian
+    yy=xx-J\func(h,xx,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,et,etpre,t); %Newton
+    xx=yy;
+    a=abs((xx-xxpre)./xx);
+    a=max(a);
+    if iterations==10
+        disp('Did not converged')
+    end
+    end
+end
+
+% This function computes the Jacobian
+function [J]=Jacob(h,xx,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,et,etpre,t)
+s=size(xx);
+s=s(1);
+delta=1e-6;
+i=0;
+J=zeros(s,s);
+while i<s
+    i=i+1;
+    f0=func(h,xx,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,et,etpre,t);
+    yy=xx;
+    yy(i)=xx(i)+delta;
+    f1=func(h,yy,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,et,etpre,t);
+    J(:,i)=(f1-f0)/delta;
+end
+end
+
+% This function determines the predictor formulation
+function [xxp]=predictor(xx,xpre1,et,etpre,t,tpre1,h,betha1,betha2)
+    xxp=xx+h*(((betha1)*evaleval(xx,t,et))+((betha2)*evaleval(xpre1,tpre1,etpre)));
+end
+
+
+%This function determines the corrector formulation
+function [f]=func(h,xx,xpre1,xpre2,alpha1,alpha2,alpha0,betha1,betha2,tpre1,et,etpre,t) 
+f=h*(betha1*evaleval(xx,t,et)+betha2*evaleval(xpre1,tpre1,etpre))+(alpha0*xx)+(alpha1*xpre1)+(alpha2*xpre2);
+end
+
+
+%This function computes the coefficients based on the step sizes
+function [alpha0,alpha1,alpha2,betha1,betha2]=coefs(c,h,holdd)
+if c==1
+    % corrector coefficients
+    % BDF-1
+    alpha1=1;
+    alpha2=0;
+    alpha0=-1;
+    betha1=1;
+    betha2=0;
+
+elseif c==2
+    %corrector coefficients
+    % BDF-2
+    alpha1=1+h/holdd;
+    alpha2=-(h^2)/((h+holdd)*holdd);
+    alpha0=-(2*h+holdd)/(h+holdd);
+    betha1=1;
+    betha2=0;
+
+elseif c==3
+    %corrector coefficients
+    % Adams-Moulton-2
+    alpha1=1;
+    alpha2=0;
+    alpha0=-1;
+    betha1=1/2;
+    betha2=1/2; 
+
+elseif c==4
+    %predictor coefficients
+    %AB-1
+    alpha1=0;
+    alpha2=0;
+    alpha0=0;
+    betha1=1;
+    betha2=0;
+    
+elseif c==5
+    %predictor coefficients
+    %AB-1
+    alpha1=0;
+    alpha2=0;
+    alpha0=0;
+    betha1=1+(h/(holdd*2));
+    betha2=-h/(holdd*2);
+end
+end
+
+
+
+%This function computes the error estimate for predictor corrector methods (Milne's estimate)
+function [dn,r]=lote(cp,cc,yp,yy,h,holdd)
+global ETOL
+ETOL=3e-6;
+if cc==1 && cp==4
+    dn=(yy-yp)*(-1/2)/((-1/2)-(1/2));
+    dn=max(abs(dn(:)));
+    r=0.9*ETOL/(dn*h);
+    r=nthroot(abs(r),2);
+% elseif cc==1 && cp==5
+%     dn=(yy-yp)/3*(1+holdd/h);
+%     r=0.9*ETOL/(dn*h);
+%     r=nthroot(r,3);  
+% elseif cc==2 && cp==4
+%     dn=(yy-yp)/3*(1+holdd/h);
+%     r=0.9*ETOL/(dn*h);
+%     r=nthroot(r,3);
+elseif cc==2 && cp==5
+    dn=(yy-yp)*(-2/9)/((-2/9)-(5/12));
+    dn=max(abs(dn(:)));
+    r=0.9*ETOL/(dn*h);
+    r=nthroot(r,3);    
+% elseif cc==3 && cp==4
+%     dn=(yy-yp)/3*(1+holdd/h);
+%     r=0.9*ETOL/(dn*h);
+%     r=nthroot(r,3);                            
+elseif cc==3 && cp==5
+    dn=(yy-yp)/(3*(1+holdd/h));
+    dn=max(abs(dn(:)));
+    r=0.9*ETOL/dn;
+    r=nthroot(r,3);
+end
+end
+
+%This function defines the system's formulation
+function [f]=evaleval(xx,t,et)
+% example #1
+% sigma=10;
+% r=28;
+% b=8/3;
+% f(1)=sigma*(xx(2)-xx(1));
+% f(2)=(r*xx(1))-xx(2)-(xx(1)*xx(3));
+% f(3)=(xx(1)*xx(2))-(b*xx(3));
+% f=[f(1);f(2);f(3)];
+% example #2
+%f=(5/xx)-(5*t*(xx^2))-(1/xx^2);
+% example #3
+%f=-100*(xx-sin(t));
+%example %4 (Opne-loop RTE Example)
+% u=1;
+% [A,B]=basedmatrix;
+% f=A*xx+B*u;
+%example %5 (close-loop RTE Example)
+[A,B]=basedmatrix;
+f=A*xx+B*et;
+end
+
+%This function does the quantification
+function [x]=quz(x,cq)
+%Uniform Quantization
+if cq==1 
+    range=1;
+    q=16;
+    x=round(x*(2^q))*range/(2^q);
+%NonUniform Quantization
+elseif cq==2 
+    xp=x;
+    if xp>0.8
+        x=2*x;
+    else
+        x=(2/3)*x+(4/3);
+    end    
+    range=1;
+    q=10;
+    x=round(x*(2^q))*range/(2^q);
+    if xp>0.8
+        x=x/2;
+    else
+        x=((3/2)*x)-2;
+    end
+end
+end
+
+%System's eigenvalues
+function [A,B]=basedmatrix
+a=-0.2;
+b=0.9;
+A=[2*a sqrt(a^2+b^2);-sqrt(a^2+b^2) 0];
+B=[-sqrt(a^2+b^2) 0]';
+end
